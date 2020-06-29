fb8b97138c1aab00a49b5b768c18f620650d3bd5
Testu <tetsu99soccer@gmail.com>
2020-06-29 20:08:32 +0900

    Add: add new

---
+++ src/views/New.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="editor">
+    <hi>New Memo</hi>
+    <textarea name="nemo"></textarea>
+    <button>保存</button>
+  </div>
+</template>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 10em;
+}
+
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+</style>
