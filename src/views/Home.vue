2302f335c184a70ec164819296de51cbc20b50ce
Testu <tetsu99soccer@gmail.com>
2020-07-06 23:34:57 +0900

    Add: imprement home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="class">
+    <ul>
+      <li v-for="memo in newest" :key="memo.id">
+        <router-link :to="{name: 'Edit', params: {id: memo.id}}">
+          {{ memo.body }}
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "home",
+  computed: {
+    newest: function() {
+      return this.$store.state.memos.slice().reverse();
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    display: block;
+    border-bottom: 1px solid #ccc;
+    padding: 20px;
+    color: inherit;
+    text-decoration: none;
+    text-align: left;
+    margin-bottom: 5px;
+  }
+</style>
